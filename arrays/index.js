613fd951bdc9f9670fc34da3c34257ac3f2e2109
Vortex127 <mirza.business15@gmail.com>
2025-01-20 23:56:36 +0500

    Learned how the map property works

--- arrays/index.js
+++ arrays/index.js
@@ -19,6 +19,42 @@ let fruits = ["apple", "orange", "banana", "mango"];
 //     console.log(fruit);
 // }
 
-fruits.sort().reverse(); //.sort used to sort in alphabetical order and .reverse to reverse sort it
-console.log(fruits);
+// fruits.sort().reverse(); //.sort used to sort in alphabetical order and .reverse to reverse sort it
+// console.log(fruits);
 
+
+// .map() is used to create a new array by calling a function on each element in the array (uses callbacks)
+
+// const numbers = [1, 2, 3, 4, 5];
+// const squares = numbers.map(square);
+// console.log(...squares);
+// const cube = numbers.map(cubes);
+// console.log(...cube);
+
+// function square(element){
+//     return Math.pow(element,2)
+// }
+
+// function cubes(element){
+//     return Math.pow(element,3)
+// }
+
+const students = ["Asad", "Tahir", "Kaiser", "Hamza", "Usman"]
+const upper = students.map(upperCase)
+const lower = students.map(lowerCase)
+const capital = students.map(capitalize)
+console.log(...upper);
+console.log(...lower);
+console.log(...capital);
+
+function upperCase(element){
+    return element.toUpperCase();
+}
+
+function lowerCase(element){
+    return element.toLowerCase();
+}
+
+function capitalize(element){
+    return element.charAt(0).toUpperCase() + element.slice(1).toLowerCase();
+}
