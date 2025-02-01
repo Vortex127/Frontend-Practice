1581bacacaf78cbd48f011b048ee88fc667c5fa1
Vortex127 <mirza.business15@gmail.com>
2025-02-01 23:12:29 +0500

    Learned what objects do

---
+++ objects/index.js
@@ -0,0 +1,26 @@
+// objects refer to a collection of related data items. Objects are used to store data that is related to each other. Objects are also used to store data that is related to each other. This is all part of OOP.
+
+
+const person1 = {
+    firstName: "Vortex",
+    lastName: "Khan",
+    age: 21,
+    occupation: "Student",
+    isEmployed: false,
+    sayHello: () => console.log("Hello")
+}
+
+const person2 = {
+    firstName: "Dead",
+    lastName: "Fish",
+    age: 18,
+    occupation: "Student",
+    isEmployed: false,
+    sayHello: () => console.log("Hellulululu Oh ma gawd")
+}
+
+person1.sayHello();
+person2.sayHello();
+console.log(person1.firstName);
+console.log(person2.lastName);
+
