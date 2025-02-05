b16217ccea95d4f12058841a0bbc9b7587962a6d
Vortex127 <mirza.business15@gmail.com>
2025-02-05 17:23:01 +0500

    Learned about static keyword

--- constructors&classes/index.js
+++ constructors&classes/index.js
@@ -34,45 +34,99 @@
 // classes below:
 
 
-class Products {
-    constructor(name, price, quantity) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
+// class Products {
+//     constructor(name, price, quantity) {
+//         this.name = name;
+//         this.price = price;
+//         this.quantity = quantity;
+//     }
+
+//     displayProducts(){
+//         console.log(`Product name: ${this.name}, Product price: $${this.price}, Product quantity: ${this.quantity}`);
+//     }
+
+//     calculateTotal(salesTax){
+//         return this.price + (this.price * salesTax);
+//     }
+// }
+
+// const salesTax = 0.05;
+// let total;
+
+// const product1 = new Products("Laptop", 1000.56, 5);
+// product1.displayProducts();
+// total = product1.calculateTotal(salesTax);
+// console.log(`Total price of product 1(with tax): $${total.toFixed(2)}`);
+
+// console.log("\n");
+
+
+// const product2 = new Products("Phone", 500.07, 10);
+// product2.displayProducts();
+// total = product2.calculateTotal(salesTax);
+// console.log(`Total price of product 2: $${total.toFixed(2)}`);
+
+// console.log("\n");
+
+
+// const product3 = new Products("Tablet", 200.45, 3);
+// product3.displayProducts();
+// total = product3.calculateTotal(salesTax);
+// console.log(`Total price of product 3 (with tax): $${total.toFixed(2)}`);
+
+
+
+// static keyword is used to declare a method that is shared by all instances of the class. The static methods are owned by the class itself hence requiring no instances Examples below:
+
+
+// class MathUtil{
+//     static PI = 3.14159;
+//     static getDiameter(radius){
+//         return 2 * radius;
+//     }
+//     static getCircumference(radius){
+//         return 2 * this.PI * radius;
+//     }
+//     static getArea(radius){
+//         return this.PI * radius ** 2;
+//     }
+// }
+
+// // const meth = new MathUtil(); no need for this as static keyword is used.
+// console.log(MathUtil.PI); 
+// console.log(MathUtil.getDiameter(5.7896).toFixed(2));
+// console.log(MathUtil.getCircumference(5.7896).toFixed(2));
+// console.log(MathUtil.getArea(9.76).toFixed(2));
+
+
+class User {
+    static userCount = 0;
+    
+    constructor(username){
+        this.username = username;
+        User.userCount++;
     }
 
-    displayProducts(){
-        console.log(`Product name: ${this.name}, Product price: $${this.price}, Product quantity: ${this.quantity}`);
+    sayHello(){
+        console.log(`Hello, my username is ${this.username}`);
     }
 
-    calculateTotal(salesTax){
-        return this.price + (this.price * salesTax);
+    static getUserCount(){
+        console.log(`There are ${User.userCount} users online`);
+        
     }
 }
+const user1 = new User("Vortex");
+const user2 = new User("Asfand");
+const user3 = new User("Booster")
 
-const salesTax = 0.05;
-let total;
-
-const product1 = new Products("Laptop", 1000.56, 5);
-product1.displayProducts();
-total = product1.calculateTotal(salesTax);
-console.log(`Total price of product 1(with tax): $${total.toFixed(2)}`);
-
-console.log("\n");
+user1.sayHello();
+user2.sayHello();
+user3.sayHello();
+User.getUserCount();
 
 
-const product2 = new Products("Phone", 500.07, 10);
-product2.displayProducts();
-total = product2.calculateTotal(salesTax);
-console.log(`Total price of product 2: $${total.toFixed(2)}`);
 
-console.log("\n");
-
-
-const product3 = new Products("Tablet", 200.45, 3);
-product3.displayProducts();
-total = product3.calculateTotal(salesTax);
-console.log(`Total price of product 3 (with tax): $${total.toFixed(2)}`);
 
 
 
