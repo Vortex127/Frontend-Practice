a2bde85597d4451cc73a05f509e31dda636b621b
Vortex127 <mirza.business15@gmail.com>
2025-03-12 01:20:19 +0500

    Learned how getters setters work

--- OOP_concepts/index.js
+++ OOP_concepts/index.js
@@ -253,7 +253,106 @@
 
 // getters & setters are used to access and modify the properties of an object.
 
+// class Rectangle {
+//     constructor(width, height){
+//         this.width = width;
+//         this.height = height;
+//     }
 
+//     set width(newWidth){
+//         if(newWidth > 0){
+//             this._width = newWidth;
+//         }
+//         else{
+//             console.error("Width must be greater than 0");
+//         }
+//     }
 
+//     set height(newHeight){
+//         if(newHeight > 0){
+//             this._height = newHeight;
+//         }
+//         else{
+//             console.error("Height must be greater than 0");
+//         }
+//     }
 
+//     get width (){
+//         return this._width;
+//     }
 
+//     get height(){
+//         return this._height;
+//     }
+
+//     get area(){
+//         return this._width * this._height;
+//     }
+// }
+
+// const rectangle = new Rectangle(10, 5);
+
+// rectangle.width = 20;
+// rectangle.height = 10;
+
+// console.log(rectangle.width);
+// console.log(rectangle.height);
+// console.log(rectangle.area);
+
+class Person {
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    set firstName(newFirstName){
+        if(typeof newFirstName === "string" && newFirstName.length > 0){
+            this._firstName = newFirstName;
+        }
+        else{
+            console.error("First name must be a non-empty string");
+        }
+    }
+
+    set lastName(newLastName){
+        if(typeof newLastName === "string" && newLastName.length > 0){
+            this._lastName = newLastName;
+        }
+        else{
+            console.error("Last name must be a non-empty string");
+        }
+    }
+
+    set age(newAge){
+        if (typeof newAge === "number" && newAge > 0){
+            this._age = newAge;
+        }
+        else{
+            console.error("Age must be a positive number");
+        }
+    }
+
+    get firstName(){
+        return this._firstName;
+    }
+
+    get lastName(){
+        return this._lastName;
+    }
+
+    get fullName(){
+        return this._firstName + " " + this._lastName;
+    }
+
+    get age(){
+        return this._age;
+    }
+
+}
+
+const person = new Person("John", "Doe", 30);
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
