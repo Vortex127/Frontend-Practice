7fd051dae38cce50dc9689e6625c23352b3a3579
Vortex127 <mirza.business15@gmail.com>
2026-04-03 17:48:06 +0500

    hello

---
+++ js_fundamentals/variables/index.js
@@ -0,0 +1,44 @@
+
+// variables below!!
+
+let x = 100;
+console.log(x);
+console.log(typeof x);
+console.log(`You are ${x} years old`);
+console.log(`Your price is $${x}`);
+
+let firstName = "Mirza"
+let favoriteFood = "Pizza"
+let email = "hello123@gmail.com"
+
+let online = false;
+let forSale = true;
+let isStudent = true;
+
+console.log(`Bro's online: $(online)`);
+console.log(`Bro's for sale: $(forSale)`);
+console.log("Enrolled: $(isStudent)");
+
+let Name = "Asfand"
+let Age = 20;
+let Country = "Pakistan";
+
+// type conversions!!
+
+let age = window.prompt("How old are you? ");
+age = Number(age); // now it works normally
+age+=1; //appends 1 to the input instead of adding 1 to the number
+console.log(age, typeof age);
+
+let x1 = "pizza"; //we replace pizza with 0 and empty strings.
+let y = "pizza";
+let z = "pizza";
+
+x1 = Number(x1);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
